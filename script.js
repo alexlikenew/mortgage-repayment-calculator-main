8f4467059eb19078b75ac4e61135bb7ab11e564c
Olek <alexlikenew@gmail.com>
2024-11-11 14:40:23 +0100

    Js logic logic completed

--- script.js
+++ script.js
@@ -3,34 +3,30 @@ const mortgageAmountInput = document.querySelector('#mortgageAmount')
 const mortgageTermInput = document.querySelector('#mortgageTerm')
 const mortgageRateInput = document.querySelector('#mortgageRate')
 const mortgageCheckbox = document.querySelectorAll('.main-box__left__form__input--type__checkbox--holder')
-
-// console.log(mortgageAmountInput.nextElementSibling);
-
 const checkform = () => {
 	if (mortgageAmountInput.value === '') {
 		mortgageAmountInput.nextElementSibling.style.display = 'block'
-		console.log(mortgageAmountInput.value)
-        
-	}else{
-        mortgageAmountInput.nextElementSibling.style.display = 'none'
-    }
+	} else {
+		mortgageAmountInput.nextElementSibling.style.display = 'none'
+	}
 	if (mortgageTermInput.value === '') {
 		mortgageTermInput.nextElementSibling.style.display = 'block'
-		console.log('Hello')
-	}else{
-        mortgageTermInput.nextElementSibling.style.display = 'none'
-    }
+	} else {
+		mortgageTermInput.nextElementSibling.style.display = 'none'
+	}
 	if (mortgageRateInput.value === '') {
 		mortgageRateInput.nextElementSibling.style.display = 'block'
-		console.log('Hello')
-	}else{
-        mortgageRateInput.nextElementSibling.style.display = 'none'
-    }
+	} else {
+		mortgageRateInput.nextElementSibling.style.display = 'none'
+	}
+}
+const calculate = () => {
+	const interestAmount = ((mortgageRateInput.value * mortgageTermInput.value) / 100) * mortgageAmountInput.value
 }
 const calculateMortgage = () => {
 	checkform()
+	calculate()
 }
-
 function handleForm(event) {
 	event.preventDefault()
 }
